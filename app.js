9d524ef4cdb69d6395c9bf1ff06c358b9e7a75c5
Bonnie Simon <bonniesimon@protonmail.com>
2019-08-10 13:33:45 +0530

    Added the functionality.

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const API_KEY = data.key;
 
 
-
+//DOm manipulation
+let temperature_element = document.querySelector('.temperature h1');
+let summary_element = document.querySelector('.summary h1');
+// console.log(temperature_element.innerHTML);
 
 // let s,temp;
 fetch(`https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/${API_KEY}/42.3601,-71.0589`)
     .then(res => res.json())
     .then( data => {
         console.log(data);
-        let temp = data.hourly.data[1].temperature;
-        let summary = data.currently.summary;
+        // let temp = data.hourly.data[1].temperature;
+        temperature_element.innerHTML = data.currently.temperature;
+        summary_element.innerHTML = data.currently.summary;
         // console.log(summary);
         
     });
